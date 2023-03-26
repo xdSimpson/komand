eba7de56b66f5243146f92d795d951292cd8f99c
Sonya Tuzhilkina <tuzhilkina_s02@mail.ru>
2023-03-27 00:18:12 +0300

    added showmenu

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "bookstore.h"
+#include <iostream>
+
+void showMenu() {
+    std::cout << "1. Add book" << std::endl;
+    std::cout << "2. Show a list of books" << std::endl;
+    std::cout << "3. Buy book" << std::endl;
+    std::cout << "4. Show a list of purchased books" << std::endl;
+    std::cout << "5. Exit" << std::endl;
+}
