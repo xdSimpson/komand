e5b489b87d5e55660afff591a7a3655161bf5067
AlexBlack1338 <like4311@gmail.com>
2023-03-27 00:34:45 +0300

    added cases 1 and 2

--- main.cpp
+++ main.cpp
@@ -8,3 +8,29 @@ void showMenu() {
     std::cout << "4. Show a list of purchased books" << std::endl;
     std::cout << "5. Exit" << std::endl;
 }
+
+int main() {
+
+    Bookstore bookstore;
+    int choice;
+
+    do {
+        showMenu();
+        std::cin >> choice;
+
+        try {
+
+            switch (choice) {
+
+            case 1: {
+                std::string title, author;
+                double price;
+                std::cout << "Введите название, автора и цену книги: ";
+                std::cin >> title >> author >> price;
+                bookstore.addBook(Book(title, author, price));
+                break;
+            }
+            case 2: {
+                bookstore.printBooks();
+                break;
+            }
